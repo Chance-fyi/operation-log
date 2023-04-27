29198bc34ce51a9f47a67cde6aa06fd9366ef50c
Chance <885046048@qq.com>
2023-04-27 17:27:45 +0800

    test: laravel orm db unit testing

---
+++ tests/illuminate/DbTest.php
@@ -0,0 +1,250 @@
+<?php
+/**
+ * Created by PhpStorm
+ * Date 2023/4/27 14:56
+ */
+
+namespace Chance\Log\Test\illuminate;
+
+use Chance\Log\facades\OperationLog;
+use Illuminate\Database\Capsule\Manager;
+use function PHPUnit\Framework\assertEmpty;
+use function PHPUnit\Framework\assertEquals;
+
+class DbTest extends TestCase
+{
+    public function testCreated()
+    {
+        $data = mockData();
+        Manager::table('user')->insert($data);
+        array_unshift($data, 1);
+        $log = createLog($data);
+
+        $data = mockData();
+        Manager::table('user')->insertOrIgnore($data);
+        array_unshift($data, 2);
+        $log .= createLog($data);
+
+        $data = mockData();
+        $id = Manager::table('user')->insertGetId($data);
+        array_unshift($data, $id);
+        $log .= createLog($data);
+
+        assertEquals(OperationLog::getLog(), trim($log));
+        assertEmpty(OperationLog::getLog());
+
+        Manager::table('user')->insert(mockData());
+        OperationLog::clearLog();
+        assertEmpty(OperationLog::getLog());
+    }
+
+    public function testBatchCreated()
+    {
+        $data = mockDatas();
+        Manager::table('user')->insert($data);
+        $log = batchCreateLog($data) . PHP_EOL;
+
+        $data = mockDatas();
+        Manager::table('user')->insertOrIgnore($data);
+        $log .= batchCreateLog($data);
+
+        assertEquals(OperationLog::getLog(), trim($log));
+    }
+
+    public function testUpdated()
+    {
+        $old = (array)Manager::table('user')->find(1);
+        $new = mockData();
+        Manager::table('user')->where('id', $old['id'])->update($new);
+        $log = updateLog($old, $new);
+
+        assertEquals(OperationLog::getLog(), trim($log));
+    }
+
+    public function testBatchUpdated()
+    {
+        $old = Manager::table('user')->where('id', '<=', 5)->get()->map(fn($v) => (array)$v)->toArray();
+        $new = mockData();
+        Manager::table("user")->where("id", "<=", 5)->update($new);
+        $log = batchUpdateLog($old, $new);
+
+        assertEquals(OperationLog::getLog(), trim($log));
+    }
+
+    public function testDeleted()
+    {
+        $old = (array)Manager::table('user')->find(1);
+        Manager::table('user')->delete($old['id']);
+        $log = deleteLog($old);
+
+        $old = (array)Manager::table('user')->find(2);
+        Manager::table('user')->where('id', $old['id'])->delete();
+        $log .= deleteLog($old);
+
+        assertEquals(OperationLog::getLog(), trim($log));
+    }
+
+    public function testBatchDeleted()
+    {
+        $old = Manager::table('user')->where('id', '<=', 5)->get()->map(fn($v) => (array)$v)->toArray();
+        Manager::table('user')->where('id', '<=', 5)->delete();
+        $log = batchDLog($old);
+
+        $old = Manager::table('user')->get()->map(fn($v) => (array)$v)->toArray();
+        Manager::table('user')->delete();
+        $log .= batchDLog($old);
+
+        assertEquals(OperationLog::getLog(), trim($log));
+    }
+
+    public function testJson()
+    {
+        $data = mockData();
+        $data['json'] = json_encode($data, JSON_UNESCAPED_UNICODE);
+        $id = Manager::table('user')->insertGetId($data);
+        array_unshift($data, $id);
+        $log = createLog($data);
+
+        $old = (array)Manager::table('user')->find($id);
+        $old['json->name'] = json_decode($old['json'], true)['name'];
+        $new = mockData();
+        $new = [
+            'json->name' => $new['name']
+        ];
+        Manager::table('user')->where('id', $id)->update($new);
+        $log .= updateLog($old, $new);
+
+        $data = mockData();
+        $data['json'] = json_encode(['data' => $data], JSON_UNESCAPED_UNICODE);
+        $id = Manager::table('user')->insertGetId($data);
+        array_unshift($data, $id);
+        $log .= createLog($data);
+
+        $old = (array)Manager::table('user')->find($id);
+        $old['json->data->name'] = json_decode($old['json'], true)['data']['name'];
+        $new = mockData();
+        $new = [
+            'json->data->name' => $new['name']
+        ];
+        Manager::table('user')->where('id', $id)->update($new);
+        $log .= updateLog($old, $new);
+
+        assertEquals(OperationLog::getLog(), trim($log));
+    }
+
+    public function testOther()
+    {
+        $old = (array)Manager::table('user')->first();
+        Manager::table('user')->where('id', $old['id'])->increment('age');
+        $log = updateLog($old, ['age' => '`age` + 1']);
+
+        $old = (array)Manager::table('user')->first();
+        Manager::table('user')->where('id', $old['id'])->increment('age', 5);
+        $log .= updateLog($old, ['age' => '`age` + 5']);
+
+        $old = (array)Manager::table('user')->first();
+        Manager::table('user')->where('id', $old['id'])->decrement('age');
+        $log .= updateLog($old, ['age' => '`age` - 1']);
+
+        $old = (array)Manager::table('user')->first();
+        Manager::table('user')->where('id', $old['id'])->decrement('age', 5);
+        $log .= updateLog($old, ['age' => '`age` - 5']);
+
+        $old = (array)Manager::table('user')->first();
+        $new = ['name' => 'Chance'];
+        Manager::table("user")->where("id", $old['id'])->decrement('age', 5, $new);
+        $new['age'] = '`age` - 5';
+        $log .= updateLog($old, $new);
+
+        assertEquals(OperationLog::getLog(), trim($log));
+    }
+
+    public function testMultipleDatabases()
+    {
+        $data = mockData();
+        $id = Manager::table("user")->insertGetId($data);
+        array_unshift($data, $id);
+        $log = createLog($data);
+
+        $data = mockData();
+        $id = Manager::connection("default1")->table("user")->insertGetId($data);
+        array_unshift($data, $id);
+        $log .= vsprintf('创建 用户1 (id:%s)：姓名1：%s，手机号1：%s，邮箱1：%s，性别1：%s，年龄1：%s', $data);
+
+        assertEquals(OperationLog::getLog(), trim($log));
+    }
+
+    public function testTransaction()
+    {
+        Manager::beginTransaction();
+            $data = mockData();
+            $id = Manager::table("user")->insertGetId($data);
+            array_unshift($data, $id);
+            $log = createLog($data);
+        Manager::commit();
+        assertEquals(OperationLog::getLog(), trim($log));
+
+        Manager::beginTransaction();
+            $data = mockData();
+            Manager::table("user")->insertGetId($data);
+        Manager::rollback();
+        assertEmpty(OperationLog::getLog());
+
+        Manager::beginTransaction();
+            $data = mockData();
+            $id = Manager::table("user")->insertGetId($data);
+            array_unshift($data, $id);
+            $log = createLog($data);
+
+            Manager::beginTransaction();
+                $data = mockData();
+                Manager::table("user")->insertGetId($data);
+            Manager::rollback();
+        Manager::commit();
+        assertEquals(OperationLog::getLog(), trim($log));
+
+        Manager::beginTransaction();
+            $data = mockData();
+            $id = Manager::table("user")->insertGetId($data);
+            array_unshift($data, $id);
+            $log = createLog($data);
+
+            Manager::beginTransaction();
+                $data = mockData();
+                $id = Manager::table("user")->insertGetId($data);
+                array_unshift($data, $id);
+                $log .= createLog($data);
+            Manager::commit();
+        Manager::commit();
+        assertEquals(OperationLog::getLog(), trim($log));
+
+        Manager::beginTransaction();
+            $data = mockData();
+            Manager::table("user")->insertGetId($data);
+
+            Manager::beginTransaction();
+                $data = mockData();
+                Manager::table("user")->insertGetId($data);
+            Manager::commit();
+        Manager::rollback();
+        assertEmpty(OperationLog::getLog());
+
+        Manager::beginTransaction();
+            $data = mockData();
+            $id = Manager::table("user")->insertGetId($data);
+            array_unshift($data, $id);
+            $log = createLog($data);
+
+            Manager::beginTransaction();
+                $data = mockData();
+                Manager::table("user")->insertGetId($data);
+
+                Manager::beginTransaction();
+                    $data = mockData();
+                    Manager::table("user")->insertGetId($data);
+                Manager::commit();
+            Manager::rollback();
+        Manager::commit();
+        assertEquals(OperationLog::getLog(), trim($log));
+    }
+}
---
+++ tests/illuminate/TestCase.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm
+ * Date 2023/4/27 14:52
+ */
+
+namespace Chance\Log\Test\illuminate;
+
+use Chance\Log\orm\illuminate\MySqlConnection;
+use Illuminate\Database\Capsule\Manager;
+use Illuminate\Database\Connection;
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    public static function setUpBeforeClass(): void
+    {
+        include_once __DIR__ . '/../function.php';
+        self::connections();
+        self::truncateTable();
+    }
+
+    private static function connections(): void
+    {
+        $capsule = new Manager();
+        $capsule->addConnection([
+            "driver" => "mysql",
+            "host" => "mysql",
+            "database" => "test",
+            "username" => "root",
+            "password" => "root",
+            "charset" => "utf8",
+            "collation" => "utf8_unicode_ci",
+            "prefix" => "tb_",
+        ]);
+        $capsule->addConnection([
+            "driver" => "mysql",
+            "host" => "mysql1",
+            "database" => "test1",
+            "username" => "root",
+            "password" => "root",
+            "charset" => "utf8",
+            "collation" => "utf8_unicode_ci",
+            "prefix" => "tb_",
+        ], "default1");
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
+        Connection::resolverFor('mysql', function ($connection, $database, $prefix, $config) {
+            return new MySqlConnection($connection, $database, $prefix, $config);
+        });
+    }
+
+    private static function truncateTable(): void
+    {
+        Manager::select("truncate table tb_user;");
+        Manager::connection("default1")->select("truncate table tb_user;");
+    }
+}
